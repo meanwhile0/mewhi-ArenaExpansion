d66c1524f9808bf1582fe30d5f9f519199eb3d95
meanwhile0 <nightmarex91@gmail.com>
2020-04-07 14:21:59 +0100

    Added a settlement check when a session is launched. This should fix the behaviour from not adding dialogues to the settlement the player loads in to.

--- MWAXArenaMaster.cs
+++ MWAXArenaMaster.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameMenus;
 using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.Library;
+using TaleWorlds.Localization;
 using SandBox;
 
 namespace ArenaExpansion {
     public class MWAXArenaMaster : CampaignBehaviorBase {
         CampaignGameStarter campaignGame;
         List<CultureObject> visitedCultures = new List<CultureObject>();
+        int previousLoadout = 0;
 
         public override void RegisterEvents() {
             CampaignEvents.SettlementEntered.AddNonSerializedListener((object)this, new Action<MobileParty, Settlement, Hero>(this.OnSettlementEntered));
@@ -25,6 +28,10 @@ namespace ArenaExpansion {
         public void OnSessionLaunched(CampaignGameStarter campaignGameStarter) {
             this.campaignGame = campaignGameStarter;
             this.AddPreDialogues(campaignGameStarter);
+            
+            if (Settlement.CurrentSettlement != null && Settlement.CurrentSettlement.IsTown) {
+                this.AddLoadoutDialogues(this.campaignGame, Settlement.CurrentSettlement);
+            }
         }
 
         public void OnSettlementEntered(MobileParty mobileParty, Settlement settlement, Hero hero) {
@@ -37,10 +44,14 @@ namespace ArenaExpansion {
             // Pre-weapons list
             campaignGameStarter.AddPlayerLine("mwax_arena_choose_weapon", "arena_master_enter_practice_fight_confirm", "mwax_arena_weapons_list", "{=mwax_arena_1}I'll choose my gear.", (ConversationSentence.OnConditionDelegate)null, (ConversationSentence.OnConsequenceDelegate)null, 200, (ConversationSentence.OnClickableConditionDelegate)null, (ConversationSentence.OnPersuasionOptionDelegate)null);
             campaignGameStarter.AddDialogLine("mwax_arena_choose_weapons_master_confirm", "mwax_arena_weapons_list", "mwax_arena_weapons_list", "{=mwax_arena_2}Alright, which weapon are you taking?", (ConversationSentence.OnConditionDelegate)null, (ConversationSentence.OnConsequenceDelegate)null, 100, (ConversationSentence.OnClickableConditionDelegate)null);
+
+            // Post-match list
+            campaignGameStarter.AddPlayerLine("mwax_arena_rematch_previous", "arena_master_post_practice_fight_talk", "close_window", "{=mwax_arena_3}Yeah. I'll take my previous loadout.", (ConversationSentence.OnConditionDelegate)null, new ConversationSentence.OnConsequenceDelegate(this.MWAX_conversation_join_arena_with_previous_loadout), 200, (ConversationSentence.OnClickableConditionDelegate)null, (ConversationSentence.OnPersuasionOptionDelegate)null);
+            campaignGameStarter.AddPlayerLine("mwax_arena_rematch_new", "arena_master_post_practice_fight_talk", "mwax_arena_weapons_list", "{=mwax_arena_4}Sure. I'll take a new loadout.", (ConversationSentence.OnConditionDelegate)null, (ConversationSentence.OnConsequenceDelegate)null, 200, (ConversationSentence.OnClickableConditionDelegate)null, (ConversationSentence.OnPersuasionOptionDelegate)null);
+
         }
 
         protected void AddLoadoutDialogues(CampaignGameStarter campaignGameStarter, Settlement settlement) {
-            InformationManager.DisplayMessage(new InformationMessage(settlement.Culture.Name.ToString()));
             // Populate weapons list
             if (!visitedCultures.Contains(settlement.Culture)) {
                 CharacterObject characterObject = Game.Current.ObjectManager.GetObject<CharacterObject>("weapon_practice_stage_1_" + settlement.Culture.StringId);
@@ -81,7 +92,12 @@ namespace ArenaExpansion {
             }
         }
 
+        public void MWAX_conversation_join_arena_with_previous_loadout() {
+            this.MWAX_conversation_join_arena_with_selected_loadout(this.previousLoadout);
+        }
+
         public void MWAX_conversation_join_arena_with_selected_loadout(int loadout) {
+            this.previousLoadout = loadout;
             Mission.Current.GetMissionBehaviour<MWAXArenaWeaponSwapLogic>().MWAXLoadoutSelect = true;
             Mission.Current.GetMissionBehaviour<MWAXArenaWeaponSwapLogic>().MWAXLoadout = loadout;
             Campaign.Current.ConversationManager.ConversationEndOneShot += new Action(MWAXArenaMaster.StartPlayerPracticeAfterConversationEnd);
